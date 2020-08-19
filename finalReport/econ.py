efafbf53b094533acde8a3fe5e740353d0254187
yut7G8 <billion7g8.316@keio.jp>
2020-08-19 13:50:05 +0900

    finalReport

---
+++ finalReport/econ.py
@@ -0,0 +1,34 @@
+#! Python3 
+# outPut.py - 計量経済学概論期末レポートをPythonで実装
+
+# ライブラリのインポート
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt 
+# 不要な警告を非表示にする
+import warnings
+warnings.filterwarnings('ignore')
+
+# データの読み込み
+df = pd.read_csv('stock2018v2.csv')
+print(df)
+
+# 散布図行列を出力
+df_pickup = df.loc[:, ['sg','r','yg','iv','cpi']]
+import seaborn as sns
+sns.pairplot(df_pickup, size=2.0)
+plt.show()
+
+# 相関係数行列を出力
+print(df.corr())
+# ヒートマップの表示
+plt.figure(figsize=(12, 9))
+sns.heatmap(df_pickup.corr(), annot=True, square=True, fmt='.2f')
+plt.show()
+
+# 単回帰
+# 説明変数X,目的変数yをarrayに変換。sklearnはarray型にしか対応していない。
+r = np.array(df.loc[:, ['r']])
+yg = np.array(df.loc[:, ['yg']])
+sg = np.array(df.loc[:, ['sg']])
